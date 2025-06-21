43da86aef32b3b923f3e23450af999c9f60fb09f
ABHI-Theq <abhi03085e@gmail.com>
2025-06-22 02:37:25 +0530

    added scripts

--- backend/Backend/socket/socket.js
+++ backend/Backend/socket/socket.js
@@ -7,7 +7,7 @@ const app=express()
 const server=http.createServer(app)
 const io=new Server(server,{
     cors:{
-        origin:'http://localhost:4000',
+        origin:'https://connect-mern-chat-app.vercel.app/',
         methods: ["GET", "POST"]
 
     }
